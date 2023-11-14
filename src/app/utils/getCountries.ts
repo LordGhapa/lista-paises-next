cc0494a1d69f2a63f390e7db834f32758c9c0af4
Lord Ghapa <felipef32@hotmail.com>
2023-11-14 06:48:19 -0300

    🚀 Deploy: teste

--- src/app/utils/getCountries.ts
+++ src/app/utils/getCountries.ts
@@ -1,6 +1,20 @@
 import { Country } from "@/types";
+async function fetchWithTimeout(resource:any, options:any = {}) {
+  const { timeout = 10000 } = options;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+
+  return response;
+}
 
 export async function getCountries(): Promise<Country[]> {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+  const res = await fetchWithTimeout("https://restcountries.com/v3.1/all");
   return res.json();
 }
